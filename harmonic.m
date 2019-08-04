46289b10e02b842eb8018d9cfde04bc6fd6f0283
docCesar <1105086063@qq.com>
2019-08-05 04:52:30 +0900

    Basic plots

--- harmonic.m
+++ harmonic.m
@@ -1,13 +1,61 @@
 clear;close all;clc;
 
-b=importfile("B4T3_10nm_Py_5nm_harmonic900(mT)_300(K)_7(Vrms)_.txt",5,900);
+fileName=dir(fullfile('*.txt'));
+%fileName= dir(fullfile('d:/datafile','*.txt'));             
+%The path should be changed to what you want.
+number=length(fileName);
+thicknessForTag=string;
+magneticForTag=string;
 
+for i=1:number
+    % Here we can add an if to separate 2 types of measurements.
+    patternTh = '(?<=Py_)\w*(?=nm_)';
+    patternMag = '(?<=harmonic)\-?\w*(?=\(mT\))';
+    thickness=regexp(fileName(i).name,patternTh, 'match');
+    thicknessForTag(end+1)=thickness{1};
+    magneticField=regexp(fileName(i).name,patternMag, 'match');
+%     magneticForTag(end+1)=str2num(magneticField{1});
+    magneticForTag(end+1)=magneticField{1};
+    isNegative=~isempty(strfind(magneticField{1},'-'));
+    if isNegative==1
+        magneticField{1}=strrep(magneticField{1},'-','N');
+        
+    end
+    
+    y = importfile(fileName(i).name);
+    eval(['b4t3_10nm_Py_' , thickness{1} , 'nm_' , magneticField{1} ,...
+        'mT=y;'])
+    figure(1)
+    plot(y(:,1),y(:,3))
+    hold on
+    grid on
+    figure(2)
+    plot(y(:,1),y(:,2))
+    hold on
+    grid on
+    figure
+%     plot(y(:,1),y(:,2),y(:,1),y(:,3));
+    plot(y(:,1),y(:,3))
+    
+    grid on
+    
+end
 
+thicknessForTag(1)=[];
+magneticForTag(1)=[];
 
-function y = importfile(filename, thickness, magneticField, startRow, endRow)
+figure(1)
+title("2nd harmonic");
+legend(magneticForTag);
+
+figure(2)
+title("1st harmonic");
+legend(magneticForTag);
+
+function y = importfile(filename, startRow, endRow)
 %% Initialization
 delimiter = '\t';
-if nargin<=4
+if nargin<=2
     startRow = 2;
     endRow = inf;
 end
