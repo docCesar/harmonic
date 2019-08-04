1e927ad1cab573a0aca1fcd6341fe83fcc05205e
docCesar <1105086063@qq.com>
2019-08-05 01:27:24 +0900

    Modified data and import function

---
+++ harmonic.m
@@ -0,0 +1,38 @@
+clear;close all;clc;
+
+
+
+
+
+function importModifiedData = importfile(filename, thickness, magneticField, startRow, endRow)
+%% Initialization
+delimiter = '\t';
+if nargin<=4
+    startRow = 2;
+    endRow = inf;
+end
+
+%% Format
+
+formatSpec = '%f%f%f%[^\n\r]';
+
+%% Open the data
+fileID = fopen(filename,'r');
+
+%% Read the data column.
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'TextType', 'string', 'HeaderLines', startRow(1)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+for block=2:length(startRow)
+    frewind(fileID);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'TextType', 'string', 'HeaderLines', startRow(block)-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
+    for col=1:length(dataArray)
+        dataArray{col} = [dataArray{col};dataArrayBlock{col}];
+    end
+end
+
+%% Close the data.
+fclose(fileID);
+
+%% Name the data column.
+eval(["b4t3_10nm_Py_", thickness , "_" , magneticField , "mT = [dataArray{1:end-1}];"])
+
+end
