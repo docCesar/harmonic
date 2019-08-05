2dc568a20495cccbf85a042347c714f2a8bb2244
docCesar <1105086063@qq.com>
2019-08-05 10:11:41 +0900

    Finished plots and debug

--- harmonic.m
+++ harmonic.m
@@ -3,42 +3,61 @@ clear;close all;clc;
 fileName=dir(fullfile('*.txt'));
 %fileName= dir(fullfile('d:/datafile','*.txt'));             
 %The path should be changed to what you want.
+
 number=length(fileName);
 thicknessForTag=string;
 magneticForTag=string;
 
+magneticForNum=[];
+maxR2ndHarmonic=[];
+
 for i=1:number
     % Here we can add an if to separate 2 types of measurements.
+    % Extract the thickness and magnetic field.
     patternTh = '(?<=Py_)\w*(?=nm_)';
     patternMag = '(?<=harmonic)\-?\w*(?=\(mT\))';
     thickness=regexp(fileName(i).name,patternTh, 'match');
     thicknessForTag(end+1)=thickness{1};
     magneticField=regexp(fileName(i).name,patternMag, 'match');
-%     magneticForTag(end+1)=str2num(magneticField{1});
+    
+    magneticForNum(end+1)=str2num(magneticField{1});
     magneticForTag(end+1)=magneticField{1};
-    isNegative=~isempty(strfind(magneticField{1},'-'));
-    if isNegative==1
-        magneticField{1}=strrep(magneticField{1},'-','N');
-        
-    end
     
     y = importfile(fileName(i).name);
+    
+    isNegative=~isempty(strfind(magneticField{1},'-'));
+    
+    if isNegative==1
+        magneticField{1}=strrep(magneticField{1},'-','N');
+        maxR2ndHarmonic(end+1)=min(y(:,3));
+    else
+        maxR2ndHarmonic(end+1)=max(y(:,3));
+    end
+    
     eval(['b4t3_10nm_Py_' , thickness{1} , 'nm_' , magneticField{1} ,...
         'mT=y;'])
+    
+    
+    
+    % Plot
     figure(1)
     plot(y(:,1),y(:,3))
     hold on
     grid on
+    
     figure(2)
     plot(y(:,1),y(:,2))
     hold on
     grid on
-    figure
-%     plot(y(:,1),y(:,2),y(:,1),y(:,3));
-    plot(y(:,1),y(:,3))
     
+    figure
+    plot(y(:,1),y(:,3))
+    title(['The 2nd harmonic result of ',char(magneticForTag(i+1)),'mT'])
+    xlabel('H (mT)')
+    ylabel('Votage (V)')
     grid on
     
+    clearvars y isNegative magneticField thickness
 end
 
 thicknessForTag(1)=[];
@@ -47,10 +66,21 @@ magneticForTag(1)=[];
 figure(1)
 title("2nd harmonic");
 legend(magneticForTag);
+xlabel('H (mT)')
+ylabel('Votage (V)')
 
 figure(2)
 title("1st harmonic");
 legend(magneticForTag);
+xlabel('H (mT)')
+ylabel('Votage (V)')
+
+figure
+scatter(magneticForNum,maxR2ndHarmonic)
+xlabel('H (mT)')
+ylabel('Votage (V)')
+box on
+grid on
 
 function y = importfile(filename, startRow, endRow)
 %% Initialization
